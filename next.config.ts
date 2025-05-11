d9455243c92c912f86d455ac526cab408686b8bb
Initfan <feeinitfan@gmail.com>
2025-05-11 22:12:52 +0700

    Config NextJs image remote pattern

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "jygqe5edc8.ufs.sh",
+				port: "",
+				search: "",
+			},
+		],
+	},
 };
 
 export default nextConfig;
