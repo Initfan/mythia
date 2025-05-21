b32031ba5ab2d75786f9596b121fc47d017ffa59
Initfan <feeinitfan@gmail.com>
2025-05-21 13:41:51 +0700

    browse, library and search navigation

--- src/components/ui/navigation.tsx
+++ src/components/ui/navigation.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import { Button } from "./button";
 import { Search, User2 } from "lucide-react";
 import { verifySession } from "@/lib/dal";
+import { Input } from "./input";
 
 interface props {
 	children?: React.ReactNode;
@@ -16,26 +17,37 @@ const Navigation = async ({ children, noLink = false }: props) => {
 
 	return (
 		<nav className="flex items-center justify-between py-5 absolute px-[5%] w-full m-auto shadow-md">
-			<Link href={"/"} className="flex items-center space-x-2">
-				<Image
-					src={MythiaLogo}
-					alt="mythia application logo"
-					width={60}
-				/>
-				<h3 className="text-xl font-semibold">Mythia</h3>
-			</Link>
+			<div className="space-x-12 flex items-center">
+				<Link href={"/"} className="flex items-center space-x-2">
+					<Image
+						src={MythiaLogo}
+						alt="mythia application logo"
+						width={60}
+					/>
+					<h3 className="text-xl font-semibold">Mythia</h3>
+				</Link>
+				<div className="space-x-6">
+					<Link
+						href="/browse"
+						className="text-sm font-semibold hover:underline"
+					>
+						Temukan
+					</Link>
+					<Link
+						href="/library"
+						className="text-sm font-semibold hover:underline"
+					>
+						Library
+					</Link>
+				</div>
+			</div>
 			{!noLink && (
 				<>
-					<div className="flex space-x-8">
-						<Link href="/">Originals</Link>
-						<Link href="/">Discover</Link>
-						<Link href="/">Trending</Link>
-						<Link href="/">Contests</Link>
-					</div>
 					<div className="flex space-x-3">
-						<Button variant={"ghost"}>
+						<Button variant="outline">
 							<Search />
 						</Button>
+						<Input placeholder="Cari novel..." />
 						{session.isAuth && (
 							<Link href={"/write"}>
 								<Button>Tulis</Button>
