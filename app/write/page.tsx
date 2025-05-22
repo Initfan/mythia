1b528592422a53b9efd967530015e06736920258
Initfan <feeinitfan@gmail.com>
2025-05-23 06:06:36 +0700

    create novel

--- app/write/page.tsx
+++ app/write/page.tsx
@@ -3,21 +3,23 @@
 import { useState } from "react";
 import StepWrite from "@/components/write/step-write";
 import WriterProfile from "@/components/write/writer-profile";
+import WriteNovel from "@/components/write/write-novel";
 
 const page = () => {
-	const [active, setActive] = useState<number>(1);
+	const [active, setActive] = useState<number>(3);
 
 	const setActivePage = (id: number) => setActive(id);
-	console.log(active);
 
 	return (
 		<>
 			<StepWrite
-				active={1}
+				active={active}
 				step={["Profile", "Buat Novel", "Tulis Cerita"]}
 			/>
 			<main className="space-x-8 flex w-1/2 mx-auto">
 				{active == 1 && <WriterProfile activePage={setActivePage} />}
+				{active == 2 && <WriteNovel activePage={setActivePage} />}
+				{active == 3 && <p>test</p>}
 			</main>
 		</>
 	);
--- src/components/write/write-novel.tsx
+++ src/components/write/write-novel.tsx
@@ -1,42 +1,279 @@
 import { FileUploadThing } from "@/components/write/file-upload";
-import SelectGenre from "@/components/write/SelectGenre";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { useContext, useEffect, useRef, useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormField, FormItem, FormLabel, FormMessage } from "../ui/form";
+import {
+	Select,
+	SelectContent,
+	SelectGroup,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "../ui/select";
+import { uploadFiles } from "@/lib/uploadthing";
+import { genre } from "@/generated/prisma";
+import { userContext } from "../../context/user-context";
+import { Textarea } from "../ui/textarea";
+import { Loader2Icon, Send } from "lucide-react";
+import { Button } from "../ui/button";
 
-const WriteNovel = () => {
+const schema = z.object({
+	title: z.string().min(4),
+	genre: z.string(),
+	synopsis: z.string(),
+	target_audience: z.enum(["male", "female"]),
+	content_rating: z.enum(["semua umur", "6+", "12+", "17+", "21+"]),
+});
+
+const WriteNovel = ({ activePage }: { activePage: (id: number) => void }) => {
+	const user = useContext(userContext);
+	const tagRef = useRef<HTMLInputElement>(null);
+	const [tag, setTag] = useState<string[]>([]);
+	const [genre, setGenre] = useState<genre[]>();
+	const [files, setFile] = useState<File[]>([]);
+	const form = useForm<z.infer<typeof schema>>({
+		resolver: zodResolver(schema),
+		defaultValues: {
+			title: "",
+			content_rating: "semua umur",
+			genre: "",
+			synopsis: "",
+			target_audience: "male",
+		},
+	});
+
+	const onSubmit = async (values: z.infer<typeof schema>) => {
+		const image = await uploadFiles("imageUploader", {
+			files,
+		});
+		const author = await fetch("api/novel", {
+			method: "POST",
+			body: JSON.stringify({
+				...values,
+				cover: image[0].ufsUrl,
+				authorId: user!.author!.id,
+				tag_novel: tag,
+			}),
+		});
+
+		const res = await author.json();
+		if (res.status == 201) {
+			activePage(3);
+		}
+	};
+
+	const onUpload = (file: File) => setFile([file]);
+
+	const fetchGenre = async () => {
+		const req = await fetch("api/genre");
+		const res = await req.json();
+		setGenre(res.data);
+	};
+
+	useEffect(() => {
+		fetchGenre();
+	}, []);
+
 	return (
-		<>
-			<div className="space-y-4 flex flex-col">
-				<h1 className="text-xl font-semibold">Cover novel</h1>
-				<div className="h-[300px] w-[200px]">
-					<FileUploadThing />
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className="w-full">
+				<div className="flex space-x-4 w-full mb-4">
+					<div className="space-y-4 flex flex-col">
+						<h1 className="text-xl font-semibold">Cover novel</h1>
+						<div className="h-[300px] w-[200px]">
+							<FileUploadThing onUpload={onUpload} />
+						</div>
+					</div>
+					<div className="flex-1 space-y-4">
+						<h1 className="text-xl font-semibold">Detail novel</h1>
+						<FormField
+							control={form.control}
+							name="title"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Judul</FormLabel>
+									<Input {...field} />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="synopsis"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Sinopsis</FormLabel>
+									<Textarea {...field} />
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="genre"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Genre</FormLabel>
+									<Select
+										name={field.name}
+										defaultValue={field.value}
+										onValueChange={field.onChange}
+									>
+										<SelectTrigger className="w-full">
+											<SelectValue placeholder="Pilih" />
+										</SelectTrigger>
+										<SelectContent>
+											<SelectGroup>
+												{genre?.map((v) => (
+													<SelectItem
+														value={v.genre}
+														key={v.genre}
+													>
+														{v.genre}
+													</SelectItem>
+												))}
+											</SelectGroup>
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="target_audience"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Target Audience</FormLabel>
+									<Select
+										name={field.name}
+										defaultValue={field.value}
+										onValueChange={field.onChange}
+									>
+										<SelectTrigger className="w-full">
+											<SelectValue placeholder="Pilih" />
+										</SelectTrigger>
+										<SelectContent>
+											<SelectGroup>
+												<SelectItem value={"male"}>
+													Male
+												</SelectItem>
+												<SelectItem value={"female"}>
+													Female
+												</SelectItem>
+											</SelectGroup>
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="content_rating"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Rating Konten</FormLabel>
+									<Select
+										name={field.name}
+										defaultValue={field.value}
+										onValueChange={field.onChange}
+									>
+										<SelectTrigger className="w-full">
+											<SelectValue placeholder="Pilih" />
+										</SelectTrigger>
+										<SelectContent>
+											<SelectGroup>
+												<SelectItem
+													value={"semua umur"}
+												>
+													Semua Umur
+												</SelectItem>
+												<SelectItem value={"6+"}>
+													6+
+												</SelectItem>
+												<SelectItem value={"12+"}>
+													12+
+												</SelectItem>
+												<SelectItem value={"17+"}>
+													17+
+												</SelectItem>
+												<SelectItem value={"21+"}>
+													21+
+												</SelectItem>
+											</SelectGroup>
+										</SelectContent>
+									</Select>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<div className="space-y-2">
+							<FormLabel>Tag</FormLabel>
+							<div className="flex space-x-4">
+								<Input
+									ref={tagRef}
+									onKeyDown={(e) => {
+										if (e.key == "Enter") {
+											e.preventDefault();
+											setTag((p) => [
+												...p,
+												tagRef.current!.value,
+											]);
+										}
+									}}
+								/>
+								<Button
+									onClick={(e) => {
+										e.preventDefault();
+										const avail = tag.includes(
+											tagRef.current!.value
+										);
+										if (!avail)
+											setTag((p) => [
+												...p,
+												tagRef.current!.value,
+											]);
+									}}
+								>
+									<Send />
+								</Button>
+							</div>
+							<div className="flex space-x-2">
+								{tag?.map((v) => (
+									<Button
+										key={v}
+										size="sm"
+										onClick={(e) => {
+											e.preventDefault();
+											const remain = tag.filter(
+												(p) => p !== v
+											);
+											setTag(remain);
+										}}
+									>
+										{v}
+									</Button>
+								))}
+							</div>
+						</div>
+					</div>
 				</div>
-			</div>
-			<div className="flex-1 space-y-4">
-				<h1 className="text-xl font-semibold">Detail novel</h1>
-				<div className="space-y-2">
-					<Label htmlFor="title">Judul</Label>
-					<Input id="title" />
-				</div>
-				<div className="space-y-2">
-					<Label htmlFor="title">Sinopsis</Label>
-					<Textarea />
-				</div>
-				<div className="space-y-2">
-					<Label htmlFor="title">Genre</Label>
-					<SelectGenre />
-				</div>
-				<div className="space-y-2">
-					<Label htmlFor="title">Target Audience</Label>
-					<Input id="title" />
-				</div>
-				<div className="space-y-2">
-					<Label htmlFor="title">Content Rating</Label>
-					<Input id="title" />
-				</div>
-			</div>
-		</>
+				<Button
+					type="submit"
+					className="w-full"
+					disabled={form.formState.isSubmitting}
+				>
+					{form.formState.isSubmitting && (
+						<Loader2Icon className="animate-spin" />
+					)}
+					Selanjutnya
+				</Button>
+			</form>
+		</Form>
 	);
 };
 
