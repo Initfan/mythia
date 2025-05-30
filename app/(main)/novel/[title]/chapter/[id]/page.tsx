992353866d207c011498cff271db3a45e15025e2
Initfan <feeinitfan@gmail.com>
2025-05-30 20:41:35 +0700

    views n comment chapter

--- app/(main)/novel/[title]/chapter/[id]/page.tsx
+++ app/(main)/novel/[title]/chapter/[id]/page.tsx
@@ -1,7 +1,10 @@
 import BreadcrumbChapter from "@/components/novel/breadcrumb-chapter";
 import ChapterComment from "@/components/novel/chapter-comment";
+import { Button } from "@/components/ui/button";
 import prisma from "@/lib/prisma";
 import parse from "html-react-parser";
+import { Eye, MessageCircle } from "lucide-react";
+import Link from "next/link";
 
 const page = async ({
 	params,
@@ -22,15 +25,53 @@ const page = async ({
 		},
 	});
 
+	await prisma.novel_chapter.update({
+		where: {
+			id: novel?.chapter.at(0)?.id,
+		},
+		data: {
+			views: {
+				increment: 1,
+			},
+		},
+	});
+
 	if (!novel?.chapter) return null;
 
 	return (
-		<div className="space-y-4">
-			<BreadcrumbChapter title={title} id={id} />
+		<div className="space-y-8">
+			<div className="flex justify-between items-center">
+				<BreadcrumbChapter title={title} id={id} />
+				<div className="space-x-3 flex">
+					<p className="text-muted-foreground flex items-center space-x-1">
+						<Eye size={18} />
+						<span>{novel.chapter.at(0)?.views}</span>
+					</p>
+					<p className="text-muted-foreground flex items-center space-x-1">
+						<MessageCircle size={18} />
+						<span>{novel.chapter.at(0)?.comment.length}</span>
+					</p>
+					<p>
+						Penulis{" "}
+						<Link
+							href={`/author/${novel.author.pen_name}`}
+							className="font-semibold hover:underline hover:text-blue-500"
+						>
+							{novel.author.pen_name}
+						</Link>
+					</p>
+				</div>
+			</div>
 			<div className="space-y-4">
-				<h1 className="text-5xl text-center">{novel.title}</h1>
+				<h1 className="text-5xl text-center">
+					{novel.chapter.at(0)?.title}
+				</h1>
 				<div className="leading-loose text-lg">
 					{parse(novel.chapter.at(0)?.content || "")}
+				</div>
+				<div className="flex justify-center space-x-2">
+					<Button variant="outline">Daftar Bab</Button>
+					<Button>Bab Selanjutnya</Button>
 				</div>
 			</div>
 			<ChapterComment
