0735dca65cdbd66d909be63db7e89298e121fa69
Initfan <feeinitfan@gmail.com>
2025-06-01 14:10:10 +0700

    color style

--- src/components/home/popular-novel.tsx
+++ src/components/home/popular-novel.tsx
@@ -3,13 +3,14 @@ import React from "react";
 import Cover from "../cover";
 import { Dot } from "lucide-react";
 import Link from "next/link";
+import { Badge } from "../ui/badge";
 
 const PopularNovel = async () => {
 	const novel = await prisma.novel.findMany({ include: { chapter: true } });
 
 	return (
 		<>
-			<h1 className="text-3xl font-semibold mb-4">Popular</h1>
+			<h1 className="text-3xl font-semibold mb-4">Populer</h1>
 			<div className="grid lg:grid-cols-3 md:grid-cols-2 gap-4">
 				{novel.map((v) => (
 					<Link
@@ -19,22 +20,23 @@ const PopularNovel = async () => {
 					>
 						<Cover src={v.cover} alt={v.title} />
 						<div className="space-y-3 flex-1 flex justify-between flex-col">
-							<h3 className="text-2xl font-semibold group-hover:underline">
+							<h3 className="text-2xl font-semibold group-hover:underline line-clamp-2">
 								{v.title}
 							</h3>
-							<p className="text-sm text-purple-600 flex space-x-4">
-								{v.genre}{" "}
-								<span className="text-primary flex space-x-4">
+							<p className="text-sm flex">
+								<Badge>{v.genre}</Badge>{" "}
+								<span className="flex space-x-4">
 									<Dot /> On going
 								</span>
 							</p>
 							<p className="text-muted-foreground line-clamp-3">
 								{v.synopsis}
 							</p>
-							<p className="text-sm text-muted flex">
-								{v.views} views
+							<p className="text-sm flex">
 								<Dot />
-								{v.chapter.length} Chapters
+								{v.views} Dilihat
+								<Dot />
+								{v.chapter.length} Bab
 							</p>
 						</div>
 					</Link>
