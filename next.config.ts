a3a3db09107b13913644c636739206635e82b44e
Initfan <feeinitfan@gmail.com>
2025-06-07 18:05:59 +0700

    unsplash remote image

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: ["jygqe5edc8.ufs.sh", "picsum.photos", "cdn.jsdelivr.net"],
 		remotePatterns: [
 			{
 				protocol: "https",
-				hostname: "jygqe5edc8.ufs.sh",
-				port: "",
-				search: "",
-			},
-			{
-				protocol: "https",
-				hostname: "picsum.photos",
-			},
-			{
-				protocol: "https",
-				hostname: "cdn.jsdelivr.net",
+				hostname: "images.unsplash.com",
 			},
 		],
 	},
