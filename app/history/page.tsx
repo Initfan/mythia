4adeda47a27c39969a0410dcf4f439debfe285bf
Initfan <feeinitfan@gmail.com>
2025-06-08 18:52:01 +0700

    history reading page

---
+++ app/history/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<div className="space-y-4">
+			<h1 className="text-3xl font-semibold">Riwayat Bacaan</h1>
+		</div>
+	);
+};
+
+export default page;
